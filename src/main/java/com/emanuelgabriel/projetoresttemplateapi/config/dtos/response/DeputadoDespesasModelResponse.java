1a59f10e86615eab17f95ed2c0f52a2a1b239a30
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-01-28 13:49:05 -0300

    removendo o modelo de representação de deputados

--- src/main/java/com/emanuelgabriel/projetoresttemplateapi/config/dtos/response/DeputadoDespesasModelResponse.java
+++ src/main/java/com/emanuelgabriel/projetoresttemplateapi/config/dtos/response/DeputadoDespesasModelResponse.java
@@ -5,18 +5,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.emanuelgabriel.projetoresttemplateapi.model.DeputadoDespesa;
+import com.emanuelgabriel.projetoresttemplateapi.model.Link;
 
 public class DeputadoDespesasModelResponse implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private List<DeputadoDespesa> dados = new ArrayList<>();
+	private List<Link> links = new ArrayList<>();
 
 	public DeputadoDespesasModelResponse() {
 	}
 
 	public List<DeputadoDespesa> getDados() {
 		return dados;
+	}
+
+	public List<Link> getLinks() {
+		return links;
 	}
 
 }

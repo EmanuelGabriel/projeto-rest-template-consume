947b78a1bdb25f947638f43855d921a30b98b63c
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-02-28 13:10:05 -0300

    refatorando o arquivo application.properties

--- src/main/java/com/emanuelgabriel/projetoresttemplateapi/config/WebClientConfig.java
+++ src/main/java/com/emanuelgabriel/projetoresttemplateapi/config/WebClientConfig.java
@@ -8,18 +8,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
 	
-	private static final String BASE_URL = "http://localhost:8081/api/";
-	private static final String BASE_URL_CAMARA_DEPUTADOS = "https://dadosabertos.camara.leg.br/api/";
 	// https://dadosabertos.camara.leg.br/api/v2/deputados/204554
+	private static final String BASE_URL_CAMARA_DEPUTADOS = "https://dadosabertos.camara.leg.br/api/";
 	
-	// @Bean
-	public WebClient webClientProdutos(WebClient.Builder builder) {
-		return builder
-			.baseUrl(BASE_URL)
-			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-			.build();
-	}
 
 	@Bean
 	public WebClient webClientCamaraDeputados(WebClient.Builder builder) {
